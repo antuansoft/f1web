9f08f53751007cfa08ba9a62dc6648bbc3b8db27
Antuansoft <amartinezalf@yahoo.es>
2022-10-24 22:48:23 +0200

    Race time calculation gap and interval

--- js/utils.js
+++ js/utils.js
@@ -3,11 +3,35 @@ function getRaceTime(currentTime){
     console.log(currentTime)
     var minutes = Math.floor(currentTime / 60000);
     console.log(minutes)
-    var seconds = ((currentTime % 60000) / 1000).toFixed(0);
+    minutes =  minutes = ('00' + minutes).slice(-2); //complete with 0s 
+    var seconds = ((currentTime % 60000) / 1000).toFixed(1);
+    console.log(seconds)
+    var dotindex = seconds.indexOf(".")
+    if (dotindex > -1)
+        seconds = seconds.substring(0,dotindex)
     console.log(seconds )
+    seconds = ('00' + seconds).slice(-2); //complete with 0s 
+    console.log(seconds)
     var miliseconds = Math.floor(((currentTime / 1000) - Math.floor(currentTime / 1000)) * 1000)  
-    miliseconds = ('0000' + miliseconds).slice(-3); //round to 3 decimals 
-    console.log( Math.floor(((currentTime / 1000) - Math.floor(currentTime / 1000)) * 1000)  )
+    miliseconds = ('000' + miliseconds).slice(-3); //complete with 0s 
+    console.log(miliseconds)
     return minutes + ":" +  seconds + ":" + miliseconds;
+
+}
+
+function getTotalTime(driverRaceTime, lap){
+
+    var total = 0
+    console.log(lap)
+    console.log(driverRaceTime)
+    
+    //for ( var index = 0; index == lap; index++){
+    var index = 0
+    while (index <= lap){
+        total += driverRaceTime[index]
+        index++
+    }
+    //console.log(total)
+    return total
 
 }
